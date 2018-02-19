8cf756d9bbe7c765aaeb3e0095a193deb5549f6d
iyeongjun <they6687@naver.com>
2018-02-20 00:12:33 +0900

    add Room

--- simple-github/src/main/kotlin/com/androidhuman/example/simplegithub/extensions/RxJavaExtension.kt
+++ simple-github/src/main/kotlin/com/androidhuman/example/simplegithub/extensions/RxJavaExtension.kt
@@ -7,3 +7,4 @@ import io.reactivex.disposables.Disposable
  * Created by iyeongjun on 2018. 2. 18..
  */
 operator fun AutoClearedDisposable.plusAssign(disposable: Disposable) = this.add(disposable)
+operator fun AutoClearedDisposable.minusAssign(disposable: Disposable) = this.detachSelf()
