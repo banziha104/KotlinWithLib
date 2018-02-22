326581e47b42bc318789ae686aeb6361dc8606f3
iyeongjun <they6687@naver.com>
2018-02-22 14:00:45 +0900

    add ViewModel

---
+++ simple-github/src/main/kotlin/com/androidhuman/example/simplegithub/di/ApiModule.kt
@@ -0,0 +1,57 @@
+package com.androidhuman.example.simplegithub.di
+
+import com.androidhuman.example.simplegithub.api.AuthApi
+import com.androidhuman.example.simplegithub.api.GithubApi
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import retrofit2.CallAdapter
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Named
+import javax.inject.Singleton
+
+/**
+ * Created by iyeongjun on 2018. 2. 22..
+ */
+
+@Module
+class ApiModule {
+    @Provides
+    @Singleton
+    fun provideAuthApi(@Named("unauthorized")okHttpClient: OkHttpClient,
+                       callAdapter: CallAdapter.Factory,
+                       converter: Converter.Factory): AuthApi
+            = Retrofit.Builder()
+            .baseUrl("https://github.com")
+            .client(okHttpClient)
+            .addCallAdapterFactory(callAdapter)
+            .addConverterFactory(converter)
+            .build()
+            .create(AuthApi::class.java)
+
+    @Provides
+    @Singleton
+    fun provideGithubApi(@Named("authorized")okHttpClient: OkHttpClient,
+                         callAdapter: CallAdapter.Factory,
+                         converter: Converter.Factory): GithubApi
+            = Retrofit.Builder()
+            .baseUrl("https://api.github.com")
+            .client(okHttpClient)
+            .addCallAdapterFactory(callAdapter)
+            .addConverterFactory(converter)
+            .build()
+            .create(GithubApi::class.java)
+
+    @Provides
+    @Singleton
+    fun provideCallAdapterFactory() : CallAdapter.Factory = RxJava2CallAdapterFactory.createAsync()
+
+    @Provides
+    @Singleton
+    fun provideConverterFactroy() : Converter.Factory = GsonConverterFactory.create()
+
+
+}
